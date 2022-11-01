0542cbe92f88eba0033e98770323aa416188fc93
Fabianno <fabiannootavio08@gmail.com>
2022-11-01 19:08:46 -0300

    feat: add access to login api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { useContext, FormEvent, useState } from 'react';
+
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../../styles/home.module.scss';
@@ -7,9 +9,30 @@ import logoImg from '../../public/logo.svg';
 import { Input } from '../components/ui/Input';
 import { Button } from '../components/ui/Button';
 
+import { AuthContext } from '../contexts/AuthContext';
+
 import Link from 'next/link';
 
 export default function Home() {
+  
+  const { signIn } = useContext(AuthContext);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+
+  async function handleLogin(event: FormEvent){
+      event.preventDefault();
+
+      let data = {
+          email,
+          password
+      }
+
+      await signIn(data);
+  }
+
   return (
     <>
       <Head>
@@ -18,15 +41,19 @@ export default function Home() {
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt='Logo Sujeito Pizzaria'/>
         <div className={styles.login}>
-          <form>
+          <form onSubmit={handleLogin}>
             <Input
               placeholder='Digite seu e-mail'
               type='text'
+              value={email}
+              onChange={(e)=> setEmail(e.target.value)}
             />
 
             <Input
               placeholder='Digite sua senha'
               type='password'
+              value={password}
+              onChange={(e)=> setPassword(e.target.value)}
             />
 
             <Button
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -10,43 +10,44 @@ import { Button } from '../../components/ui/Button';
 import Link from 'next/link';
 
 export default function SignUp() {
-  return (
-    <>
-      <Head>
-        <title>Faça seu cadastro agora!</title>
-      </Head>
-      <div className={styles.containerCenter}>
-        <Image src={logoImg} alt='Logo Sujeito Pizzaria'/>
-        <div className={styles.login}>
-            <h1>Criando sua conta</h1>
-          <form>
-            <Input
-              placeholder='Digite seu nome'
-              type='text'
-            />
 
-            <Input
-              placeholder='Digite seu e-mail'
-              type='text'
-            />
+    return (
+        <>
+            <Head>
+                <title>Faça seu cadastro agora!</title>
+            </Head>
+            <div className={styles.containerCenter}>
+                <Image src={logoImg} alt='Logo Sujeito Pizzaria' priority/>
+                <div className={styles.login}>
+                    <h1>Criando sua conta</h1>
+                <form>
+                    <Input
+                    placeholder='Digite seu nome'
+                    type='text'
+                    />
 
-            <Input
-              placeholder='Digite sua senha'
-              type='password'
-            />
+                    <Input
+                    placeholder='Digite seu e-mail'
+                    type='text'
+                    />
 
-            <Button
-              type='submit'
-              loading={false}
-            >
-              Cadastrar
-            </Button>
-          </form>
-          <Link className={styles.text} href='/'>
-            Já possui uma conta? Faça o login
-          </Link>
-        </div>
-      </div>
-    </>
-  )
+                    <Input
+                    placeholder='Digite sua senha'
+                    type='password'
+                    />
+
+                    <Button
+                    type='submit'
+                    loading={false}
+                    >
+                    Cadastrar
+                    </Button>
+                </form>
+                <Link className={styles.text} href='/'>
+                    Já possui uma conta? Faça o login
+                </Link>
+                </div>
+            </div>
+        </>
+    )
 }
