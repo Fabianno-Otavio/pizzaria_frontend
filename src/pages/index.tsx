ef16b3bc9cac94f8a58c915b8045d44c4b795498
Fabianno <fabiannootavio08@gmail.com>
2022-11-01 16:00:13 -0300

    feat: add input component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,31 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../../styles/home.module.scss';
+import logoImg from '../../public/logo.svg';
+import { Input } from '../components/ui/Input';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <Head>
+        <title>Sujeito Pizza</title>
+      </Head>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt='Logo Sujeito Pizzaria'/>
+        <div className={styles.login}>
+          <form>
+            <Input
+              placeholder='Digite seu e-mail'
+              type='text'
+            />
+
+            <Input
+              placeholder='Digite sua senha'
+              type='password'
+            />
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
