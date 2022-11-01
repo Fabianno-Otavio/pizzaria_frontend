057028063e51856e0224152e41f886b363af395a
Fabianno <fabiannootavio08@gmail.com>
2022-11-01 17:21:36 -0300

    feat: create login component

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,24 +1,30 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../../styles/home.module.scss';
+import styles from '../../../styles/home.module.scss';
 
-import logoImg from '../../public/logo.svg';
+import logoImg from '../../../public/logo.svg';
 
-import { Input } from '../components/ui/Input';
-import { Button } from '../components/ui/Button';
+import { Input } from '../../components/ui/Input';
+import { Button } from '../../components/ui/Button';
 
 import Link from 'next/link';
 
-export default function Home() {
+export default function SignUp() {
   return (
     <>
       <Head>
-        <title>Sujeito Pizza</title>
+        <title>Faça seu cadastro agora!</title>
       </Head>
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt='Logo Sujeito Pizzaria'/>
         <div className={styles.login}>
+            <h1>Criando sua conta</h1>
           <form>
+            <Input
+              placeholder='Digite seu nome'
+              type='text'
+            />
+
             <Input
               placeholder='Digite seu e-mail'
               type='text'
@@ -33,11 +39,11 @@ export default function Home() {
               type='submit'
               loading={false}
             >
-              Acessar
+              Cadastrar
             </Button>
           </form>
-          <Link className={styles.text} href='/signup'>
-            Não possui uma conta? Cadastre-se
+          <Link className={styles.text} href='/'>
+            Já possui uma conta? Faça o login
           </Link>
         </div>
       </div>
